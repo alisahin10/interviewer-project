dece028e1cc45618c64d87423d5f5cd6eb663fdf
alisahin <alisahin10@icloud.com>
2024-04-23 01:26:52 +0300

    initial commit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -130,7 +130,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-CSRF_TRUSTED_ORIGINS=['https://interviewer-project.site', 'https://www.interviewer-project.site']
 
 LANGUAGE_CODE = 'en-us'
 
